d71dec681f494dc14cb3fd7e8661539120842b0b
Raushan Kumar <sraushan890@gmail.com>
2026-08-07 14:41:43 +0530

    fix: remove console log for launch data download error handling

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -42,7 +42,6 @@ async function populateLaunches() {
     });
 
     if (response.status !== 200) {
-      console.log("Problem downloading launch data");
       throw new Error("Launch data download failed");
     }
     const launchDocs = response.data.docs;
